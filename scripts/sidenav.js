68f867766a03a0233a2105c0e1e14baa88d26452
Ricardo Vieira <rjvmcv.98@gmail.com>
2023-03-12 15:48:31 +0000

    Cleanup

--- scripts/sidenav.js
+++
@@ -1,8 +0,0 @@
-function toggleNav() {
-  const sideNav = document.getElementById('mySidenav');
-  const btn = document.getElementById('sidenav-btn');
-  sideNav.classList.toggle('open');
-  btn.classList.toggle('clicked')
-  btn.classList.toggle('fa-bars');
-  btn.classList.toggle('fa-times');
-}
